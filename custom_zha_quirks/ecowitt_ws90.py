a39bfd502f4c1e2cef1abde512934a274933e886
Firemogle <brett.carriger@gmail.com>
2026-03-28 21:23:58 -0400

    Update ecowitt_ws90.py

--- custom_zha_quirks/ecowitt_ws90.py
+++ custom_zha_quirks/ecowitt_ws90.py
@@ -1,43 +1,43 @@
-"""Device handler for Shelly WS90 Weather Station."""
+
+"""Shelly WS90 Weather Station"""
 
 from __future__ import annotations
 
 from zigpy import types
 from zigpy.quirks import CustomCluster
-from zigpy.quirks.v2 import QuirkBuilder, ReportingConfig
-from zigpy.quirks.v2.homeassistant import DEGREE, UnitOfPrecipitationDepth, UnitOfSpeed
-from zigpy.quirks.v2.homeassistant.binary_sensor import BinarySensorDeviceClass
-from zigpy.quirks.v2.homeassistant.sensor import SensorDeviceClass, SensorStateClass
-from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef
+from zigpy.quirks.v2 import (
+    QuirkBuilder,
+    ReportingConfig,
+    SensorDeviceClass,
+    SensorStateClass,
+    BinarySensorDeviceClass,
+    homeassistant
+)
+from zigpy.zcl import foundation
 
 
 class ShellyWindCluster(CustomCluster):
     """Wind measurement cluster for Shelly WS90."""
 
     cluster_id = 0xFC01
-    name = "Shelly Wind Cluster"
-    ep_attribute = "shelly_wind_cluster"
 
-    class AttributeDefs(BaseAttributeDefs):
+    class AttributeDefs(foundation.BaseAttributeDefs):
         """Wind cluster attribute definitions."""
-
-        wind_speed = ZCLAttributeDef(
+        
+        wind_speed = foundation.ZCLAttributeDef(
             id=0x0000,
             type=types.uint16_t,
-            access="rp",
-            manufacturer_code=0x1490,
+            access="rp"
         )
-        wind_direction = ZCLAttributeDef(
+        wind_direction = foundation.ZCLAttributeDef(
             id=0x0004,
             type=types.uint16_t,
-            access="rp",
-            manufacturer_code=0x1490,
+            access="rp"
         )
-        gust_speed = ZCLAttributeDef(
+        gust_speed = foundation.ZCLAttributeDef(
             id=0x0007,
             type=types.uint16_t,
-            access="rp",
-            manufacturer_code=0x1490,
+            access="rp"
         )
 
 
@@ -45,17 +45,14 @@ class ShellyUVCluster(CustomCluster):
     """UV index measurement cluster for Shelly WS90."""
 
     cluster_id = 0xFC02
-    name = "Shelly UV Cluster"
-    ep_attribute = "shelly_uv_cluster"
 
-    class AttributeDefs(BaseAttributeDefs):
+    class AttributeDefs(foundation.BaseAttributeDefs):
         """UV cluster attribute definitions."""
-
-        uv_index = ZCLAttributeDef(
+        
+        uv_index = foundation.ZCLAttributeDef(
             id=0x0000,
             type=types.uint8_t,
-            access="rp",
-            manufacturer_code=0x1490,
+            access="rp"
         )
 
 
@@ -63,23 +60,19 @@ class ShellyRainCluster(CustomCluster):
     """Rain measurement cluster for Shelly WS90."""
 
     cluster_id = 0xFC03
-    name = "Shelly Rain Cluster"
-    ep_attribute = "shelly_rain_cluster"
 
-    class AttributeDefs(BaseAttributeDefs):
+    class AttributeDefs(foundation.BaseAttributeDefs):
         """Rain cluster attribute definitions."""
-
-        rain_status = ZCLAttributeDef(
+        
+        rain_status = foundation.ZCLAttributeDef(
             id=0x0000,
             type=types.Bool,
-            access="rp",
-            manufacturer_code=0x1490,
+            access="rp"
         )
-        precipitation = ZCLAttributeDef(
+        precipitation = foundation.ZCLAttributeDef(
             id=0x0001,
             type=types.uint24_t,
-            access="rp",
-            manufacturer_code=0x1490,
+            access="rp"
         )
 
 
@@ -89,74 +82,76 @@ class ShellyRainCluster(CustomCluster):
     .replaces(ShellyUVCluster)
     .replaces(ShellyRainCluster)
     .sensor(
-        attribute_name=ShellyWindCluster.AttributeDefs.wind_speed.name,
+        attribute_name="wind_speed",
         cluster_id=ShellyWindCluster.cluster_id,
         divisor=10,
-        unit=UnitOfSpeed.METERS_PER_SECOND,
+        unit=homeassistant.UnitOfSpeed.METERS_PER_SECOND,
         device_class=SensorDeviceClass.WIND_SPEED,
         state_class=SensorStateClass.MEASUREMENT,
+        translation_key="wind_speed",
+        fallback_name="Wind Speed",
         reporting_config=ReportingConfig(
             min_interval=10, max_interval=900, reportable_change=1
-        ),
-        fallback_name="Wind speed",
+        )
     )
     .sensor(
-        attribute_name=ShellyWindCluster.AttributeDefs.wind_direction.name,
+        attribute_name="wind_direction",
         cluster_id=ShellyWindCluster.cluster_id,
         divisor=10,
-        unit=DEGREE,
-        device_class=SensorDeviceClass.WIND_DIRECTION,
+        unit=homeassistant.DEGREE,
         state_class=SensorStateClass.MEASUREMENT,
+        translation_key="wind_direction",
+        fallback_name="Wind Direction",
         reporting_config=ReportingConfig(
             min_interval=10, max_interval=900, reportable_change=1
-        ),
-        fallback_name="Wind direction",
+        )
     )
     .sensor(
-        attribute_name=ShellyWindCluster.AttributeDefs.gust_speed.name,
+        attribute_name="gust_speed", 
         cluster_id=ShellyWindCluster.cluster_id,
         divisor=10,
-        unit=UnitOfSpeed.METERS_PER_SECOND,
+        unit=homeassistant.UnitOfSpeed.METERS_PER_SECOND,
         device_class=SensorDeviceClass.WIND_SPEED,
         state_class=SensorStateClass.MEASUREMENT,
-        reporting_config=ReportingConfig(
-            min_interval=10, max_interval=900, reportable_change=1
-        ),
         translation_key="gust_speed",
-        fallback_name="Gust speed",
+        fallback_name="Gust Speed",
+        reporting_config=ReportingConfig(
+            min_interval=10, max_interval=900, reportable_change=1
+        )
     )
     .sensor(
-        attribute_name=ShellyUVCluster.AttributeDefs.uv_index.name,
+        attribute_name="uv_index",
         cluster_id=ShellyUVCluster.cluster_id,
         divisor=10,
         state_class=SensorStateClass.MEASUREMENT,
+        translation_key="uv_index",
+        fallback_name="UV Index",
         reporting_config=ReportingConfig(
             min_interval=10, max_interval=900, reportable_change=1
-        ),
-        translation_key="uv_index",
-        fallback_name="UV index",
+        )
     )
     .sensor(
-        attribute_name=ShellyRainCluster.AttributeDefs.precipitation.name,
+        attribute_name="precipitation",
         cluster_id=ShellyRainCluster.cluster_id,
         divisor=10,
-        unit=UnitOfPrecipitationDepth.MILLIMETERS,
+        unit=homeassistant.UnitOfPrecipitationDepth.MILLIMETERS,
         device_class=SensorDeviceClass.PRECIPITATION,
         state_class=SensorStateClass.TOTAL_INCREASING,
+        translation_key="precipitation",
+        fallback_name="Precipitation",
         reporting_config=ReportingConfig(
             min_interval=10, max_interval=900, reportable_change=1
-        ),
-        fallback_name="Precipitation",
+        )
     )
     .binary_sensor(
-        attribute_name=ShellyRainCluster.AttributeDefs.rain_status.name,
+        attribute_name="rain_status",
         cluster_id=ShellyRainCluster.cluster_id,
         device_class=BinarySensorDeviceClass.MOISTURE,
+        translation_key="rain_status",
+        fallback_name="Rain Detected",
         reporting_config=ReportingConfig(
             min_interval=10, max_interval=900, reportable_change=1
-        ),
-        translation_key="rain_detected",
-        fallback_name="Rain detected",
+        )
     )
     .add_to_registry()
 )
